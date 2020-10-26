50b9fc60f1b0bfaff5b855900519801e0a25cc66
AnandPr123 <gyanandprakash@gmail.com>
2020-10-26 20:08:59 +0530

    added the solutioning for templateig

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,17 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-Route::post('/teams','TeamController@index');
-Route::get('/teams/{id}','TeamController@getTeam');
-Route::post('/teams/{id}/member','MemberController@addMember');
-Route::delete('/teams/{id}/members/{id2}','MemberController@deleteMember');
-Route::get('/teams/​{id}/tasks','TaskController@getTasks');
-Route::post('/teams/​{id}/tasks','TaskController@addTask');
-Route::get('/teams/​{id1}/tasks/{id2}','TaskController@getTask');
-Route::patch('/teams/​{id1}/tasks/{id2}','TaskController@patchTask');
-Route::get('/teams/​{id1}/members/{id2}/tasks/','TaskController@memberTasks');
+ */
+Route::post('/teams', 'TeamController@index');
+Route::get('/teams/{id}', 'TeamController@getTeam');
+Route::post('/teams/{id}/member', 'MemberController@addMember');
+Route::delete('/teams/{id}/members/{id2}', 'MemberController@deleteMember');
+Route::get('/teams/​{id}/tasks', 'TaskController@getTasks');
+Route::post('/teams/​{id}/tasks', 'TaskController@addTask');
+Route::get('/teams/​{id1}/tasks/{id2}', 'TaskController@getTask');
+Route::patch('/teams/​{id1}/tasks/{id2}', 'TaskController@patchTask');
+Route::get('/teams/​{id1}/members/{id2}/tasks/', 'TaskController@memberTasks');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+https: //github.com/Gyan843/TaskManagerAPI.git
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-  $visited = DB::select('select * from places where visited = ?', [1]); 
-  $togo = DB::select('select * from places where visited = ?', [0]);
+    $visited = DB::select('select * from places where visited = ?', [1]);
+    $togo = DB::select('select * from places where visited = ?', [0]);
 
-  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+    return view('travel_list', ['visited' => $visited, 'togo' => $togo]);
 });
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');
